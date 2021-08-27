e45450d822d42617bafc7d4ca2fe765df444a622
jshan98 <jshanahan@ggc.edu>
2021-08-26 22:36:12 -0400

    fixed add_nums

---
+++ 02-add_nums.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "Enter your first number"
+read first
+echo "Enter your second number"
+read second
+echo "Enter your third number"
+read third
+
+sum=$(( first+second+third ))
+
+echo "The sum is $sum"
+
+echo "Please enter the height"
+read height
+echo "Please enter the width"
+read width
+
+totalPixels=$(( height*width ))
+
+echo "The total number of pixels is $totalPixels"
+
+# exercise: ask the user for the width and height and present total
+# number of pixels

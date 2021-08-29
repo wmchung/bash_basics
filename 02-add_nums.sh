01afb4a0526156160be8ff94e2c74ed95e72979f
Ryan Gambrell <rgambrell@ggc.edu>
2021-08-29 13:30:33 -0400

    02-add_nums Parallel Exercise Completed - Prompts for Height first

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,14 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+echo ""
+echo "What is the height of your display?"
+read height
+echo ""
+echo "What is the width of your display?"
+read width
+
+echo ""
+pixelTotal=$(( $width * $height ))
+echo "Your display has ${pixelTotal} pixels." 

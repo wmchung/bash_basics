1e333efa0d96f8516934351b8ff04cbfcf6e3fd7
Steven Lopez <steven43533@gmail.com>
2025-01-27 13:00:45 -0500

    Created parallel branch and updated 09 file

--- 09-name.sh
+++ 09-name.sh
@@ -12,3 +12,8 @@ done
 # exercise: Change the list of servers and also the 
 # operation applied to them. For instance, use ping, 
 # traceroute, or nslookup with other options.
+
+for server in $servers; do
+    tracert $server
+    echo "----------------------------"
+done
